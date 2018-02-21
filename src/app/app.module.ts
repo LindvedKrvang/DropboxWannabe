5cbe48e60f9998dda8a352226ea3e2d061115b09
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2018-02-21 09:18:04 +0100

    Created toolbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import {UserModule} from './user/user.module';
 import {RouterModule} from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {SharedModule} from './shared/shared.module';
 
 
 @NgModule({
@@ -23,6 +24,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     MatButtonModule,
     AppRoutingModule,
+    SharedModule,
     AuthModule,
     HomeModule,
     UserModule,
